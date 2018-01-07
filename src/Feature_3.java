447e696e776560df7874552fc21d85acc2f983b9
Abhishek Sharma <abhiislikeme@gmail.com>
2018-01-07 22:37:20 +0530

    feature-3 update-2

--- src/Feature_3.java
+++ src/Feature_3.java
@@ -1,9 +1,11 @@
 /*
- * Actually final list after matching the elements into the Intro part and Result section part are stored into the final_list and this list may have duplicate values
- * By using the Set we remove the duplicate values from the final_list
- * Now For feature 3 we are counting the final_list.size() and introduction_section.size().
+  * Now For feature 3 we are counting the ref_list_from_result.size() and ref_list_from_intro.size().
  * And the ratio between them will give us the 3rd feature.
  *
+ */
+/*
+ * Issue : 1
+ * References are repeating into the list.
  */
 
 import java.util.ArrayList;
@@ -33,32 +35,12 @@ public class Feature_3
       {
           e.printStackTrace();
       }
-     System.out.println("Ref List from the Result Section :   ");
-     System.out.println(ref_list_from_result);
 
 
-     System.out.println("Now we are matching the elements from Introduction to result section  :  ");
-     ArrayList<String> final_list = new ArrayList<String>();
-     int counter1 = 0;
-     for(String s1 : ref_list_from_intro )
-     {
-         for(String s2 : ref_list_from_result)
-         {
-             if(s1.equals(s2))
-             {
-                 System.out.println(s1);
-                 final_list.add(s1);
-             }
-         }
-     }
-
-      Set<String> s = new HashSet<String>(final_list);
-     for(String str : s)
-     {
-         System.out.println(str);
-     }
-     counter1 = s.size();
+     int counter1 = ref_list_from_result.size();
      int counter2 = ref_list_from_intro.size();
+     //Set<String> set = new HashSet<>();
+
 
      System.out.println("feature  3  :  " + ((counter1*1.0)/counter2) );
   }
